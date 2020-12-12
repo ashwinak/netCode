b25ccd38c2999cbdc368dcd9f7a827c21d745b6f
ashwinak <ashwinak@gmail.com>
2020-12-12 11:57:02 -0800

    Update threading.py

--- threading.py
+++ threading.py
@@ -26,9 +26,9 @@ def target_function(router):
     print(output.decode())
     ssh_client.close()
 
-router1 = {'server_ip': 'localhost', 'server_port': '22', 'user':'aa', 'passwd':'blah', 'config':'ospf.txt'}
-router2 = {'server_ip': 'localhost', 'server_port': '22', 'user': 'aa', 'passwd': 'blah', 'config':'eigrp.txt'}
-router3 = {'server_ip': 'localhost', 'server_port': '22', 'user': 'aa', 'passwd': 'blah', 'config':'router3.txt'}
+router1 = {'server_ip': 'localhost', 'server_port': '22', 'user':'aa', 'passwd':'blah', 'config':'target1.txt'}
+router2 = {'server_ip': 'localhost', 'server_port': '22', 'user': 'aa', 'passwd': 'blah', 'config':'target2.txt'}
+router3 = {'server_ip': 'localhost', 'server_port': '22', 'user': 'aa', 'passwd': 'blah', 'config':'target3.txt'}
 
 devices = [router1, router2, router3]
 my_threads = list()
